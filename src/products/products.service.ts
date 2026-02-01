86bfa7216b62d7dd163caa88bae6a67767d98750
友人 <lijunpg@foxmail.com>
2026-02-02 00:36:13 +0800

    type:修改后端类型

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 // 定义数据类型
 // 首先我们要明白，一个商品有哪些属性，
-// 首先是id，商品一定有一个独一无二的id
-// 然后是名称，商品一定有一个名称
-// 然后是价格，商品一定有一个价格
-// 然后是图片，商品一定有一个图片
-// 然后是描述，商品一定有一个描述,这里先定义3个类型
 interface Product {
-  id: number; // id一般是数字类型
-  name: string; // 名称是字符串类型
-  price: number; //价格是数字类型
+  id: number;
+  title: string; // 商品标题
+  price: number; // 价格
+  coverUrl?: string; // 封面图链接
+  tags?: string[]; // 标签（比如：京东超市、包邮、看相似）
+  commentCount?: string; // 评论数（比如：100+条评论）
+  shopName?: string; // 店铺名称
 }
 
 @Injectable()
@@ -19,17 +18,17 @@ export class ProductsService {
   private Products: Product[] = [
     {
       id: 1,
-      name: 'iphone 15',
+      title: 'iphone 15',
       price: 4999,
     },
     {
       id: 2,
-      name: '小米10s',
+      title: '小米10s',
       price: 4999,
     },
     {
       id: 3,
-      name: '华为mate 20',
+      title: '华为mate 20',
       price: 8999,
     },
   ];
